1aa5437a3c869998ce9b797b32a6604ef3001401
DongQi <dongqi1177@virtuos.com.cn>
2016-10-13 16:50:11 +0800

    addt Player shoot

--- Assets/MyScripts/Player.cs
+++ Assets/MyScripts/Player.cs
@@ -8,6 +8,8 @@ public class Player : MonoBehaviour {
 
     public Animator mAnimator;
 
+    public Transform mShotPoint;
+
     // Use this for initialization
     void Start () {
         mFPSController = this.GetComponent<RigidbodyFirstPersonController>();
@@ -16,8 +18,30 @@ public class Player : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        if (Input.GetMouseButtonDown(0))
+        {
+            OnShoot();
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            OnStopShoot();
+        }
+    }
+
+    void OnShoot()
+    {
+        mAnimator.SetBool("Shoot", true);
+    }
+
+    void OnStopShoot()
+    {
+        mAnimator.SetBool("Shoot", false);
+    }
+
+    public void OnBullet()
+    {
+
+    }
 
     void OnMoveInputCallBack(Vector2 input)
     {
