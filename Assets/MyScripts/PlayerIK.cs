94cde0fed629b3720e31ad18e00a2d20db670e24
DongQi <dongqi1177@virtuos.com.cn>
2016-10-13 10:12:15 +0800

    add player IK animation

---
+++ Assets/MyScripts/PlayerIK.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerIK : MonoBehaviour {
+
+    public Transform mLookAtTarge;
+
+    private Animator mAnimator;
+
+    void Awake()
+    {
+        mAnimator = this.GetComponent<Animator>();
+    }
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnAnimatorIK(int layerIndex)
+    {
+        if(mAnimator != null)
+        {
+            mAnimator.SetLookAtWeight(1, 1, 1, 1);
+
+            if(mLookAtTarge != null)
+            {
+                mAnimator.SetLookAtPosition(mLookAtTarge.position);
+            }
+
+        }
+    }
+}
